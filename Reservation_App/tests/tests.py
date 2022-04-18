8e94c26979e1d3de5aa3f6c948829e9739559181
Baranixowy <baranix96@gmail.com>
2022-04-18 17:56:18 +0200

    models tests and views documentation

--- Reservation_App/tests/tests.py
+++ Reservation_App/tests/tests.py
@@ -1,6 +1,7 @@
+from django.urls import reverse
 from django.test import Client
 import pytest
-from Reservation_App.models import Cities
+from Reservation_App.models import Cities, SportPitches, SportMatches
 
 
 def test_main_page():
@@ -15,11 +16,20 @@ def test_cities_model(cityCracow, cityWarsaw):
     assert Cities.objects.get(name="Kraków") == cityCracow
     assert Cities.objects.get(name="Warszawa") == cityWarsaw
 
+@pytest.mark.django_db
+def test_pitch_model(createPitch):
+    assert len(SportPitches.objects.all()) == 1
+    assert SportPitches.objects.get(name="Orlik") == createPitch
 
-# @pytest.mark.django_db
-# def test_city_view(cityCracow):
-#     c = Client()
-#     city = Cities.objects.first()
-#     print(city)
-#     response = c.get('city/{city}/')
-#     assert response.status_code == 200
+@pytest.mark.django_db
+def test_match_model(createMatch):
+    assert len(SportMatches.objects.all()) == 1
+    assert SportMatches.objects.get(gamedate="2022-04-22",) == createMatch
+
+
+@pytest.mark.django_db
+def test_city_view(cityCracow):
+    c = Client()
+    url = reverse('cityview', kwargs={'city': "Kraków"})
+    response = c.get(url)
+    assert response.status_code == 200
